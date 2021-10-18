6f5e5854b5de5cb9909be481e9702302f2196c0e
mac <mac@macs-Mac-mini.local>
2021-10-18 15:53:16 -0400

    bootstrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,19 @@
 function Home(props) {
-    return <h1>Home</h1>;
+    return (
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide"></img>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide"></img>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide"></img>
+    </div>
+  </div>
+</div>
+    )
   }
   
   export default Home;
